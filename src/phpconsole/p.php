121888800162da919b84c58fbaeada77355c5c15
Peter Legierski <peter@legierski.net>
2014-03-02 20:30:54 +0000

    Port to Composer/Packagist
    Signed-off-by: Peter Legierski <peter@legierski.net>

---
+++ src/phpconsole/p.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * http://phpconsole.com
+ *
+ * A detached logging facility for PHP to aid your daily development routine.
+ *
+ * Watch quick tutorial at: https://vimeo.com/58393977
+ *
+ * @link https://github.com/phpconsole
+ * @copyright Copyright (c) 2012 - 2014 phpconsole.com
+ * @license See LICENSE file
+ * @version 2.0.0
+ */
+
+namespace phpconsole;
+
+class p {
+
+    private static $is_set_up;
+    private static $phpconsole;
+
+    public static function send($payload, $options = array())
+    {
+        self::setup();
+
+        return self::$phpconsole->send($payload, $options);
+    }
+
+    public static function sendToAll($payload, $options = array())
+    {
+        self::setup();
+
+        return self::$phpconsole->sendToAll($payload, $options);
+    }
+
+    private static function setup()
+    {
+        if(self::$is_set_up !== true) {
+
+            self::$phpconsole = new phpconsole();
+            self::$phpconsole->loadConfig(array('backtrace_depth' => 1));
+
+            self::$is_set_up = true;
+        }
+    }
+}
